a0c7568f57b960ea25a2fcb3eb3e1b1eddc641d9
Deep Hudka <deep.hudka.19041@iitgoa.ac.in>
2022-01-05 16:42:51 +0530

    Simcard select functionality added

--- app/src/main/java/com/example/smsredirect/MainActivity.kt
+++ app/src/main/java/com/example/smsredirect/MainActivity.kt
@@ -3,22 +3,21 @@ package com.example.smsredirect
 import android.Manifest
 import android.app.ActivityManager
 import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Telephony
 import android.telephony.SmsManager
-import android.widget.TextView
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import kotlinx.android.synthetic.main.activity_main.*
-import android.net.ConnectivityManager
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,29 +62,66 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     fun receiveMsg() {
         val br = object : BroadcastReceiver() {
             override fun onReceive(p0: Context?, p1: Intent?) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                val bundle = p1?.extras
+
+                //val slot = bundle!!.getInt("slot", -1)
+                var slot = -1
+                try {
+                    if (bundle != null) {
+                        val keySet = bundle.keySet()
+                        for (key in keySet) {
+                            when (key) {
+                                "slot" -> slot = bundle.getInt("slot", -1)
+                                "simId" -> slot = bundle.getInt("simId", -1)
+                                "simSlot" -> slot = bundle.getInt("simSlot", -1)
+                                "slot_id" -> slot = bundle.getInt("slot_id", -1)
+                                "simnum" -> slot = bundle.getInt("simnum", -1)
+                                "slotId" -> slot = bundle.getInt("slotId", -1)
+                                "slotIdx" -> slot = bundle.getInt("slotIdx", -1)
+                                else -> if (key.lowercase(Locale.getDefault())
+                                        .contains("slot") or key.lowercase(
+                                        Locale.getDefault()
+                                    )
+                                        .contains("sim")
+                                ) {
+                                    val value = bundle.getString(key, "-1")
+                                    if ((value == "0") or (value == "1") or (value == "2")) {
+                                        slot = bundle.getInt(key, -1)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch (e: Exception) {
+                    Log.d(TAG, "Exception=>$e")
+                }
+                //Toast.makeText(applicationContext, "slot== " + slot, Toast.LENGTH_SHORT).show()
+                var sim = 0
+                if (slot == 0) {
+                    sim = 1
+                }
+                else if(slot==1){
+                    sim = 2
+                }
+                if (sim.toString() == simcardNumber.text.toString()) {
                     for (sms in Telephony.Sms.Intents.getMessagesFromIntent(p1)) {
-                        if (sms.originatingAddress != null && sms.displayMessageBody != null){
+                        if (sms.originatingAddress != null && sms.displayMessageBody != null) {
                             phone_rec_sms.setText(sms.originatingAddress)
                             sms_body.setText(sms.displayMessageBody)
                             sendMsg()
+                        }
                     }
-                    }
+
                 }
             }
-
         }
-
         LocalBroadcastManager.getInstance(this).registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
         registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
     }
-
-
-
-
 
 
     private fun isMyServiceRunning(mClass: Class<MyService>): Boolean {
@@ -101,10 +137,6 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
-
-
-
-
     private fun sendMsg() {
         val sms = SmsManager.getDefault()
         if (sendNumber.text.toString() != "") {
@@ -115,7 +147,7 @@ class MainActivity : AppCompatActivity() {
                 null,
                 null
             )
-        }else{
+        }/*else{
             sms.sendTextMessage(
                 phone_rec_sms.text.toString(),
                 "ME",
@@ -123,7 +155,7 @@ class MainActivity : AppCompatActivity() {
                 null,
                 null
             )
-        }
+        }*/
     }
 
 }
